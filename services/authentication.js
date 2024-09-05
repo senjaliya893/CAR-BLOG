45a30f4b7ce66f58a50c4d75998db1ee8c273c4b
ViVEK SENJALiYA <154584056+senjaliya893@users.noreply.github.com>
2024-09-05 14:31:28 +0530

    complate

---
+++ services/authentication.js
@@ -0,0 +1,25 @@
+const JWT = require("jsonwebtoken");
+
+const secret = "$uperMan@123";
+
+function createTokenForUser(user) {
+  const payload = {
+    _id: user._id,
+    email: user.email,
+    profileImageURL: user.profileImageURL,
+    role: user.role,
+  };
+  console.log("PAYLOAD---------", payload)
+  const token = JWT.sign(payload, secret);
+  return token;
+}
+
+function validateToken(token) {
+  const payload = JWT.verify(token, secret);
+  return payload;
+}
+
+module.exports = {
+  createTokenForUser,
+  validateToken,
+};
